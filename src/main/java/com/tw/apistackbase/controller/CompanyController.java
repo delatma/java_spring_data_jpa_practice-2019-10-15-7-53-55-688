ccd1e1b9f28a865de6ec755dddf371123073ccd1
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-16 14:31:50 +0800

    Finish exercise

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -1,18 +1,31 @@
 package com.tw.apistackbase.controller;
 
 import com.tw.apistackbase.core.Company;
+import com.tw.apistackbase.repositories.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
+    @Autowired
+    private CompanyRepository companyRepository;
+
     @GetMapping(produces = {"application/json"})
     public Iterable<Company> list() {
-        return null;
+        return companyRepository.findAll();
     }
 
     @PostMapping(produces = {"application/json"})
     public Company add(@RequestBody Company company) {
-        return null;
+        return companyRepository.save(company);
+
+//        We don't say companyRepository.save(company); return
+//        Since the returned company does not have a value for ID
+//
+//        companyRepository.save(company); modifies company to set a value for ID
+//        according to @GeneratedValue
     }
 }
